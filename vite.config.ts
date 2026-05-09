c48987675cca05e386f45883b03d44ecc2649ff3
Henry Getz <henrysamuelgetz@gmail.com>
2026-05-09 21:44:08 +0000

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/chord-cruncher/",
   plugins: [react()],
   test: {
     exclude: ["node_modules/**", "dist/**", "web-dist/**"],
